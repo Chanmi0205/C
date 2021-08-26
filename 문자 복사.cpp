e9bb46fe8e175f3374c2b80aa070b13993027477
Chanmi0205 <84673395+Chanmi0205@users.noreply.github.com>
2021-08-26 09:53:34 +0900

    Add files via upload

---
+++ 문자 복사.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h> 
+int main(void) {
+	char input[10] = "ABC";
+	char result[5] = "Love";
+	strcpy(result, input);
+	printf("문자열 복사 : %s\n", result);
+	return 0;
+} 
