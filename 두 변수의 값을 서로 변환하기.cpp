0044018fbceb3e8f0c6d37d591751dad3d4ee215
Chanmi0205 <84673395+Chanmi0205@users.noreply.github.com>
2021-08-29 19:13:58 +0900

    Add files via upload

---
+++ 두 변수의 값을 서로 변환하기.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// 포인터 : 특정 데이터가 저장된 메모리의 주소를 저장하는 변수
+// 두 변수의 값을 서로 변환하는 포인터 함 
+
+void swap (int *x, int *y) {
+	int temp;
+	temp = *x;
+	*x = *y;
+	*y = temp;
+} 
+int main (void) {
+	int x = 1;
+	int y = 2;
+	swap(&x, &y);
+	printf ("x = %d\ny = %d\n", x, y); 
+	return 0;
+}
