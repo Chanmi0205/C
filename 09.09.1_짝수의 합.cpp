f8a3b977dccdcc02183c9b12069d0f47bac76522
Chanmi0205 <84673395+Chanmi0205@users.noreply.github.com>
2021-09-12 01:20:29 +0900

    Add files via upload

---
+++ 09.09.1_짝수의 합.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int even(int k);
+main( ) {
+	int a, sum=0;
+	for ( a=1; a<=10; a++ ) 
+	sum = sum + even(a);
+	printf ("%d", sum);
+}
+int even(int k) {
+	if ( k % 2 == 0 )
+	return k;
+	else
+	return 0;
+}
