462ac77e09551fe07c2efe9ebb5db5185ba9299e
Chanmi0205 <84673395+Chanmi0205@users.noreply.github.com>
2021-10-29 22:45:09 +0900

    Add files via upload

---
+++ 10_28_2_이진검색.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+main ( ) {
+	int m, a=1, b=10, d, cnt=1;
+	printf ("ют╥б : ");
+	scanf ("%d", &m); 
+	d = ( a + b ) / 2;  
+	while ( d != m ) { 
+		if ( d > m )    
+		b = d - 1;      
+		else 
+		a = d + 1;
+	cnt = cnt + 1; 
+	d = ( a + b ) / 2;	
+	}
+	printf ("%d", cnt);
+}
