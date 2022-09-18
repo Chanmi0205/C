7a79e9fbaad4c3e304fcfab5d50e7566e4c0feed
Chanmi0205 <84673395+Chanmi0205@users.noreply.github.com>
2022-09-18 23:29:25 +0900

    c언어 복습

---
+++ project.cpp
@@ -0,0 +1,20 @@
+// º¹½À 
+#include <stdio.h>
+
+main() {
+		int k;
+		int g[5] = {84, 76, 54, 80, 65);
+		for(k=0; k<=4; k++) {
+		  	switch(g[k]/10) {
+		  		case 10 : printf("A"); break;
+		  		case 9 : printf("A"); break;
+		  		case 8 : printf("B"); break; 
+		  		case 7 : printf("C"); break;
+		  		case 6 : printf("D");
+		  		case 5 : printf("E");
+		  		default : printf("F");
+		  	}
+		 printf("\n");
+		}
+	}
+}
